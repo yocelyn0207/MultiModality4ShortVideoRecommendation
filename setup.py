1e52d99f10c8bd4a1777d203d69e472ea55bc800
yocelyn0207 <130754892+yocelyn0207@users.noreply.github.com>
2023-10-20 16:57:44 +0800

    setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ from setuptools import setup, find_packages
 setup(
     name='MM4SVR',
     version='0.1.0',
-    author="Alice Wang",
-    author_email="alicejwong1122@gmail.com",
+    author="Xinyue Zhang",
+    author_email="yocelyn464@gmail.com",
     packages=find_packages(include=['scripts', 'scripts.*']),
     install_requires=parse_requirements('requirements.txt', session='hack'),
     extras_require={
